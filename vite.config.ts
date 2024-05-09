6268841b65971cbb3eac430b73434b759dd3fdb4
dmitriy.salangin <dmitriy.salangin@simpleone.ru>
2024-05-09 20:26:33 +0300

    chore: add tsconfig-paths plugin for using absolute paths

--- vite.config.ts
+++ vite.config.ts
@@ -1,13 +1,15 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 import stylelint from 'vite-plugin-stylelint';
-import { nodePolyfills } from 'vite-plugin-node-polyfills'
+import { nodePolyfills } from 'vite-plugin-node-polyfills';
+import tsconfigPaths from 'vite-tsconfig-paths';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
     react(),
     nodePolyfills(),
+    tsconfigPaths(),
     stylelint({
       fix: true,
       cache: false,
