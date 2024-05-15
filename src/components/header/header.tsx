df780bb606ded9c4d44872a592fbcdecd110e763
dmitriy.salangin <dmitriy.salangin@simpleone.ru>
2024-05-16 00:25:13 +0300

    feat: add menu items

--- src/components/header/header.tsx
+++ src/components/header/header.tsx
@@ -2,6 +2,14 @@ import { useState } from 'react';
 import styles from './header.module.css';
 import Logo from 'components/logo/logo';
 import cn from 'classnames';
+import basketIcon from '../../assets/basket.svg';
+import profileIcon from '../../assets/profile.svg';
+import loginIcon from '../../assets/login.svg';
+import signupIcon from '../../assets/signup.svg';
+import logoutIcon from '../../assets/logout.svg';
+import coursesIcon from '../../assets/courses.svg';
+
+
 
 type HeaderProps = {
   isAuth: boolean,
@@ -18,31 +26,58 @@ function Header({ isAuth, theme }: HeaderProps) {
         <Logo
           theme={theme}
         />
-        <nav className={cn(styles['header__nav'], { [styles.active]: isOpen })}>
-          <ul className={styles['header__navList']}>
-            <li className={styles['header__navItem']}>
-              <a href="#main">Main</a>
+        <nav className={cn(styles.header__nav, { [styles.active]: isOpen })}>
+          <ul className={cn(styles.header__navList, styles.header__siteNav)}>
+            <li className={styles.header__navItem}>
+              <a className={styles.siteNav__link} href="#main">
+              <img src={coursesIcon} alt="Courses icon" />
+                Courses
+                </a>
             </li>
-            <li className={styles['header__navItem']}>
-              <a href="#about">About</a>
+            <li className={styles.header__navItem}>
+              <a href="#about">About us</a>
+            </li>
+          </ul>
+          <ul className={cn(styles.header__navList, styles.header__userNav)}>
+            <li className={styles.header__navItem}>
+              <a className={styles.userNav__link} href="#busket">
+                <img src={basketIcon} alt="Basket icon" />
+                Basket
+              </a>
             </li>
             {isAuth
-              ? <li className={styles['header__navItem']}>
-                <button className={styles['logout-button']}>Logout</button>
-              </li>
-              : <>
-                <li className={styles['header__navItem']}>
-                  <a href="#login">Login</a>
+              ? <>
+                <li className={styles.header__navItem}>
+                  <a className={styles.userNav__link} href="#profile">
+                    <img src={profileIcon} alt="Profile icon" />
+                    Profile
+                  </a>                </li>
+                <li className={styles.header__navItem}>
+                  <button className={cn(styles.logoutButton, styles.userNav__link)}>
+                    <img src={logoutIcon} alt="Profile icon" />
+                    Logout
+                  </button>
                 </li>
-                <li className={styles['header__navItem']}>
-                  <a href="#signup">Sign up</a>
+              </>
+              : <>
+                <li className={styles.header__navItem}>
+                  <a className={styles.userNav__link}  href="#login">
+                    <img src={loginIcon} alt="Profile icon" />
+                    Login
+                  </a>
+                </li>
+                <li className={styles.header__navItem}>
+                  <a className={styles.userNav__link}  href="#signup">
+                    <img src={signupIcon} alt="Profile icon" />
+                    Sign up
+                  </a>
                 </li>
               </>
             }
           </ul>
         </nav>
         <button
-          className={cn(styles['header__menuButton'], { [styles.open]: isOpen })}
+          className={cn(styles.header__menuButton, { [styles.open]: isOpen })}
           onClick={() => setOpen(!isOpen)}
         >
           <div></div>
