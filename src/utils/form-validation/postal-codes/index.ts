ada18473522ac45645b3dd0d443fddb821e7189a
maria <ysters14@gmail.com>
2024-05-16 22:42:16 +0500

    feat: add tests to check postal codes

--- src/utils/form-validation/postal-codes/index.ts
+++ src/utils/form-validation/postal-codes/index.ts
@@ -5,7 +5,7 @@ interface PostalCode {
 }
 
 export const POSTAL_CODES_REG_EXP: PostalCode = Object.fromEntries(
-  data.postalCodeRegex.map(({ _territoryId, __text }) => [_territoryId, new RegExp(__text, 'g')]),
+  data.postalCodeRegex.map(({ _territoryId, __text }) => [_territoryId, new RegExp(`^${__text}$`)]),
 );
 
 export const COUNTRIES_CODES = Object.keys(POSTAL_CODES_REG_EXP);
