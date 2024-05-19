55dac07a5bc24f8ab9d2e852cd7b7db6d07d7e07
dmitriy.salangin <dmitriy.salangin@simpleone.ru>
2024-05-19 13:53:31 +0300

    feat: add not found page

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,12 @@
 import './styles/normalize.css';
 import './styles/variables.css';
 import './styles/global.css';
-import Header from '@components/header/';
+import NotFound from '@pages/not-found';
 
 function App() {
   return (
     <>
-      <Header isAuth={true} theme="dark" />
+      <NotFound />
     </>
   );
 }
