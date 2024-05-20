028f4ac6a2af8fd470d717ae86844bd23a259d70
dmitriy.salangin <dmitriy.salangin@simpleone.ru>
2024-05-20 23:41:56 +0300

    feat: add main page

--- src/router.tsx
+++ src/router.tsx
@@ -2,7 +2,12 @@ import { createBrowserRouter } from 'react-router-dom';
 import Layout from './components/layout';
 import Main from './pages/main';
 import Login from './pages/login';
-import SignUp from './pages/sign-up';
+import SignUp from './pages/sign-up/';
+import Courses from './pages/courses/';
+import AboutUs from './pages/about-us';
+import Profile from './pages/profile';
+import Cart from './pages/cart';
+import NotFound from './pages/not-found/';
 import CheckAuth from './components/checkAuth';
 import UserContextProvider from './contexts/userContext';
 
@@ -19,26 +24,26 @@ export const router = createBrowserRouter([
         path: '/',
         element: <Main />,
       },
-      // {
-      //   path: '/courses',
-      //   element: <Courses />,
-      // },
-      // {
-      //   path: '/about-us',
-      //   element: <AboutUs />,
-      // },
-      // {
-      //   path: '/Cart',
-      //   element: <Cart />,
-      // },
-      // {
-      //   path: '/profile',
-      //   element: <Profile />,
-      // },
-      // {
-      //   path: '*',
-      //   element: <NotFound />
-      // },
+      {
+        path: '/courses',
+        element: <Courses />,
+      },
+      {
+        path: '/about',
+        element: <AboutUs />,
+      },
+      {
+        path: '/cart',
+        element: <Cart />,
+      },
+      {
+        path: '/profile',
+        element: <Profile />,
+      },
+      {
+        path: '*',
+        element: <NotFound />,
+      },
     ],
   },
   {
