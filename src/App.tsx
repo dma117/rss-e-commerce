84e3fc4d6a80412ba31b96fff97d20c5968a2daf
Ilana Lipkina <ilanalipkina@gmail.com>
2024-05-22 14:02:46 +0400

    fix: useEffect dependency error

--- src/App.tsx
+++ src/App.tsx
@@ -26,7 +26,7 @@ function App() {
       const currentApiRoot = createAnonymousApiBuilder();
       setApiRoot(currentApiRoot);
     }
-  });
+  }, [setApiRoot, setIsUserLoggedIn]);
 
   return (
     <>
