90f540c04bd6fd61be03673dfff40ba2e9ac4d7f
dmitriy.salangin <dmitriy.salangin@simpleone.ru>
2024-06-04 23:17:39 +0300

    feat: add discount

--- src/components/product-card-min/index.tsx
+++ src/components/product-card-min/index.tsx
@@ -1,5 +1,6 @@
 import { Link } from 'react-router-dom';
 import styles from './style.module.css';
+import cn from 'classnames';
 
 type ProductCardMinProps = {
   id: string;
@@ -7,15 +8,28 @@ type ProductCardMinProps = {
   title: string;
   description: string;
   price: number;
+  finalPrice: number,
 };
 
-function ProductCardMin({ id, imgSrc, title, description, price }: ProductCardMinProps) {
+function ProductCardMin({ id, imgSrc, title, description, price, finalPrice }: ProductCardMinProps) {
   return (
     <Link className={styles.productCard} to={`./${id}`}>
+      {!!finalPrice &&
+        <span className={styles.discount}>%</span>
+      }
       <img src={imgSrc} alt="" />
       <div className={styles.title}>{title}</div>
       <div>{description}</div>
-      <div className={styles.price}>{`${price}$`}</div>
+      <div className={styles.price}>
+        {!!finalPrice &&
+          <span className={styles.finalPrice}>
+            {`${finalPrice.toFixed(2)}$`}
+          </span>
+        }
+        <span className={cn({ [styles['oldPrice']]: finalPrice })}>
+          {`${price}$`}
+        </span>
+      </div>
     </Link>
   );
 }
