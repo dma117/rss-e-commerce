1970f812079056aee47c31771f5540d17cec4fd0
dmitriy.salangin <dmitriy.salangin@simpleone.ru>
2024-06-04 22:47:22 +0300

    feat: add breadcrumbs

--- src/components/product-list/index.tsx
+++ src/components/product-list/index.tsx
@@ -3,9 +3,13 @@ import { Product } from '@commercetools/platform-sdk';
 import { useEffect, useState } from 'react';
 import styles from './style.module.css';
 import ProductCardMin from '@components/product-card-min';
+import Breadcrumbs from '../breadcrumbs';
 
 type ProductListProps = {
-  categoryId: string;
+  categoryId: 'c96ff3d0-1688-4913-90ae-a3056e259e68'
+  | '78db1a69-6023-44b5-8b3d-a8f294cdd335'
+  | 'dac8edad-bf16-4f56-859c-f364efde1c2a'
+  | '9f44fc3d-b2b9-4625-91e8-03934154b07d',
 };
 
 function ProductList({ categoryId }: ProductListProps) {
@@ -38,33 +42,38 @@ function ProductList({ categoryId }: ProductListProps) {
   }, [apiRoot, categoryId]);
 
   return (
-    <div className={styles.productList}>
-      {products &&
-        products.map((product) => {
-          const id = product.id;
-          const imgSrc =
-            product.masterData.current.masterVariant.assets &&
-            product.masterData.current.masterVariant.assets[0].sources[0].uri;
-          const title = product.masterData.current.name['en-GB'];
-          const description =
-            product.masterData.current.description &&
-            product.masterData.current.description['en-GB'];
-          const price =
-            product.masterData.current.masterVariant.prices &&
-            product.masterData.current.masterVariant.prices[0].value.centAmount / 100;
+    <div className={styles.catalog}>
+      <Breadcrumbs
+        categoryId={categoryId}
+      />
+      <div className={styles.productList}>
+        {products &&
+          products.map((product) => {
+            const id = product.id;
+            const imgSrc =
+              product.masterData.current.masterVariant.assets &&
+              product.masterData.current.masterVariant.assets[0].sources[0].uri;
+            const title = product.masterData.current.name['en-GB'];
+            const description =
+              product.masterData.current.description &&
+              product.masterData.current.description['en-GB'];
+            const price =
+              product.masterData.current.masterVariant.prices &&
+              product.masterData.current.masterVariant.prices[0].value.centAmount / 100;
 
-          return (
-            <ProductCardMin
-              key={id}
-              id={id}
-              imgSrc={imgSrc || ''}
-              title={title}
-              description={description || ''}
-              price={price || 0}
-            />
-          );
-        })}
-      {error && error}
+            return (
+              <ProductCardMin
+                key={id}
+                id={id}
+                imgSrc={imgSrc || ''}
+                title={title}
+                description={description || ''}
+                price={price || 0}
+              />
+            );
+          })}
+        {error && error}
+      </div>
     </div>
   );
 }
--- src/utils/const.ts
+++ src/utils/const.ts
@@ -21,3 +21,10 @@ export const CategoryId = {
   DESIGN: 'dac8edad-bf16-4f56-859c-f364efde1c2a',
   MARKETING: '9f44fc3d-b2b9-4625-91e8-03934154b07d',
 };
+
+export const CategoryMap = {
+  'c96ff3d0-1688-4913-90ae-a3056e259e68': 'Programming',
+  '78db1a69-6023-44b5-8b3d-a8f294cdd335': 'Business',
+  'dac8edad-bf16-4f56-859c-f364efde1c2a': 'Design',
+  '9f44fc3d-b2b9-4625-91e8-03934154b07d': 'Marketing',
+};
